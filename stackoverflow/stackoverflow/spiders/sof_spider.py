c063d739561dbf66dc93e3b2a1f21743c7da4336
gcmurillo <gcmurillo@outlook.com>
2018-08-10 23:58:21 -0500

    languages tags

--- stackoverflow/stackoverflow/spiders/sof_spider.py
+++ stackoverflow/stackoverflow/spiders/sof_spider.py
@@ -14,7 +14,25 @@ class StackoverflowItem(Item):
 
 class StackOverFlow(Spider):
     name = "StackOverFlow"
-    start_urls = ["http://es.stackoverflow.com"]
+    languages_urls = [
+        "https://es.stackoverflow.com/questions/tagged/php",
+        "https://es.stackoverflow.com/questions/tagged/javascript",
+        "https://es.stackoverflow.com/questions/tagged/java",
+        "https://es.stackoverflow.com/questions/tagged/android",
+        "https://es.stackoverflow.com/questions/tagged/html",
+        "https://es.stackoverflow.com/questions/tagged/c%23",
+        "https://es.stackoverflow.com/questions/tagged/jquery",
+        "https://es.stackoverflow.com/questions/tagged/python",
+        "https://es.stackoverflow.com/questions/tagged/android-studio"
+    ]
+
+    tab_urls = [
+        "https://es.stackoverflow.com/?tab=active",
+
+
+    ]
+
+    start_urls = tab_urls
 
     def get_extracted(value, index=0):
         try:
@@ -24,16 +42,26 @@ class StackOverFlow(Spider):
 
 
     def parse(self, response):
-        tema_extract = response.xpath('//a[@class="question-hyperlink"]/text()').extract()
-        votos_extract = response.xpath('//div[@class="votes"]//span/text()').extract()
-        vistas_extract = response.xpath('//div[@class="views"]//span/text()').extract()
-        respuestas_extract = response.xpath('//div[contains(@class, "status")]//span/text()').extract()
+        languages = ['php', 'javascript', 'java', 'android', 'html', 'c%23', 'jquery', 'python', 'android-studio']
+        language = str(response).split('/')[-1].strip('>')
+        if language in languages:
+            tema_extract = response.xpath('//a[@class="question-hyperlink"]/text()').extract()
+            votos_extract = response.xpath('//span[@class="vote-count-post "]//strong/text()').extract()
+            vistas_extract = response.xpath('//div[@class="views "]/text()').extract()
+            respuestas_extract = response.xpath('//div[contains(@class, "status")]//strong/text()').extract()
 
-        i = 0
-        while i < len(tema_extract):
-            print('Tema: ' + tema_extract[i])
-            print('Votos: ' + votos_extract[i])
-            print('Vistas: ' + vistas_extract[i])
-            print('Respuestas: ' + respuestas_extract[i])
-            print('\n')
-            i += 1
+            i = 0
+            while i < len(tema_extract):
+                print("Lenguage: " + language)
+                print('Tema: ' + tema_extract[i])
+                print('Votos: ' + votos_extract[i])
+                print('Vistas: ' + vistas_extract[i])
+                print('Respuestas: ' + respuestas_extract[i])
+                print('\n')
+                i += 1
+
+        else:
+            tema_extract = response.xpath('//a[@class="question-hyperlink"]/text()').extract()
+            i = 0
+            while i < len(tema_extract):
+                print('Tema: ' + tema_extract[i])
